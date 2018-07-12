6e664c980ffd7cfbcdd110f9ad48b86b7b33d26f
Pavel Naumov <pavliknaumov@gmail.com>
2018-07-12 09:47:54 +0100

    things work good

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,24 +8,21 @@ class OrdersController < ApplicationController
 
   def create
     category = Category.find(params[:category_id])
-    #gym = Gym.find(params[:gym_id])
-    order  = Order.create!(category_name: category.name, number_of_visits: category.number_of_visits, amount: category.price, state: 'pending', user: current_user)
+    order  = Order.create!(category_name: category.name, gym_name: category.gym.name, number_of_visits: category.number_of_visits, amount: category.price, state: 'pending', user: current_user)
 
     redirect_to new_order_payment_path(order)
   end
 end
 
-#   def show
+
+# def cart
+#     @orders_cart = current_user.orders.where(state: 'pending') #like items in F
 #     @order = Order.find(params[:id])
 #     @items = current_user.orders.where(state: 'pending')
 #     @price_total = 0
 #     for i in 0 .. @items.length - 1
 #       @price_total += (@items[i].amount_cents)/100.to_i
 #     end
-#   end
-
-# def cart
-#     @orders_cart = current_user.orders.where(state: 'pending') #like items in F
 #   end
 
 
