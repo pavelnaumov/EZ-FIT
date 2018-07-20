5ce77b43f05e8d52ce594d8e356fb56d868f1129
Pavel Naumov <pavliknaumov@gmail.com>
2018-07-20 16:46:27 +0100

    the useless page is not useful

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,6 @@ class CategoriesController < ApplicationController
   def show
     @gym = Gym.find(params[:gym_id])
     @category = Category.find(params[:id])
+    @order  = Order.create!(category_name: @category.name, gym_name: @category.gym.name, gym_photo: @category.gym.photo, number_of_visits: @category.number_of_visits, amount: @category.price, state: 'pending', user: current_user)
   end
 end
