3eda4bd1dcae48e8e01d14982282b0d3270aef0e
Pavel Naumov <pavliknaumov@gmail.com>
2018-07-24 15:47:19 +0100

    same things again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :detect_device_format
+
+  private
+  
+  def detect_device_format
+    case request.user_agent
+    when /iPad/i
+      request.variant = :tablet
+    when /iPhone/i
+      request.variant = :phone
+    when /Android/i && /mobile/i
+      request.variant = :phone
+    when /Android/i
+      request.variant = :tablet
+    when /Windows Phone/i
+      request.variant = :phone
+    end
 end
--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -2,24 +2,30 @@ class GymsController < ApplicationController
 
   def index
     if params[:query].present?
-    @gyms = Gym.where("name ILIKE?","%#{params[:query]}%")
-  else
-    @gyms = Gym.all
-    @markers = @gyms.map do |gym|
-     {
+      @gyms = Gym.where("name ILIKE?","%#{params[:query]}%")
+    else
+      @gyms = Gym.all
+      @markers = @gyms.map do |gym|
+       {
         lat: gym.latitude,
         lng: gym.longitude,
         infoWindow: {
           content: gym.name
         }
       }
+      respond_to do |format|
+        format.json
+      format.html          # /app/views/home/index.html.erb
+      format.html.phone    # /app/views/home/index.html+phone.erb
+      format.html.tablet   # /app/views/home/index.html+tablet.erb
     end
   end
 end
+end
 
-  def show
-    @gym = Gym.find(params[:id])
-    @categories = @gym.categories
-  end
+def show
+  @gym = Gym.find(params[:id])
+  @categories = @gym.categories
+end
 end
 
