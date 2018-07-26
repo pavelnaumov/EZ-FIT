26cc3a3ac9f48a222cf112c550b248499580a35e
Pavel Naumov <pavliknaumov@gmail.com>
2018-07-26 12:44:41 +0100

    emails with yahoo do not work

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
-ActionMailer::Base.smtp_settings = {
-  address: "smtp.mail.yahoo.com",
-  port: 587,
-  domain: 'yahoo.com',
-  user_name: ENV['YAHOO_ADDRESS'],
-  password: ENV['YAHOO_APP_PASSWORD'],
-  authentication: :login,
-  enable_starttls_auto: true
-}
+# ActionMailer::Base.smtp_settings = {
+#   address: "smtp.mail.yahoo.com",
+#   port: 587,
+#   domain: 'yahoo.com',
+#   user_name: ENV['YAHOO_ADDRESS'],
+#   password: ENV['YAHOO_APP_PASSWORD'],
+#   authentication: :login,
+#   enable_starttls_auto: true
+# }
